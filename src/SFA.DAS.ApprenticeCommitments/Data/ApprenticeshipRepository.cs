1035df54bd4eeea86e2ff183c517476be20d0e7f
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-09 05:33:55 +0000

    List apprenticeships for an apprentice

--- src/SFA.DAS.ApprenticeCommitments/Data/ApprenticeshipRepository.cs
+++ src/SFA.DAS.ApprenticeCommitments/Data/ApprenticeshipRepository.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 using SFA.DAS.ApprenticeCommitments.Data.Models;
 using SFA.DAS.ApprenticeCommitments.Map;
 using SFA.DAS.ApprenticeCommitments.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeCommitments.Data
 {
@@ -25,6 +28,13 @@ namespace SFA.DAS.ApprenticeCommitments.Data
             await dbContext.SaveChangesAsync();
 
             return apprenticeship.MapToApprenticeshipModel();
+        }
+
+        public async Task<List<ApprenticeshipModel>> FindByApprenticeId(Guid apprenticeId)
+        {
+            var found = await _dbContext.Value.Apprenticeships
+                .Where(a => a.Apprentice.Id == apprenticeId).ToListAsync();
+            return found.Select(a => a.MapToApprenticeshipModel()).ToList();
         }
     }
 }
