cb99470e59e3d21aae0671986682255e6548d112
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-16 11:28:54 +0000

    Move Steps files next to their features

---
+++ src/SFA.DAS.ApprenticeCommitments.Api/Controllers/ConfirmTrainingProviderController.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.ApprenticeCommitments.Api.Controllers
+{
+    public class ConfirmTrainingProviderRequest
+    {
+        public bool TrainingProviderCorrect { get; set; }
+    }
+
+    public class ConfirmTrainingProviderController
+    {
+    }
+}
