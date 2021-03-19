64395513b7240d4f33fb191ea6d11192b6fcfa0f
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-19 10:01:20 +0000

    Rename context

--- src/SFA.DAS.ApprenticeCommitments/Application/Commands/ChangeEmailAddressCommand/ChangeEmailAddressCommandHandler.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Commands/ChangeEmailAddressCommand/ChangeEmailAddressCommandHandler.cs
@@ -8,14 +8,14 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Commands.CreateRegistrationC
 {
     public class ChangeEmailAddressCommandHandler : IRequestHandler<ChangeEmailAddressCommand>
     {
-        private readonly IApprenticeContext _apprenticeRepository;
+        private readonly IApprenticeContext _apprentices;
 
-        public ChangeEmailAddressCommandHandler(IApprenticeContext apprenticeRepository)
-            => _apprenticeRepository = apprenticeRepository;
+        public ChangeEmailAddressCommandHandler(IApprenticeContext apprentices)
+            => _apprentices = apprentices;
 
         public async Task<Unit> Handle(ChangeEmailAddressCommand command, CancellationToken cancellationToken)
         {
-            var apprentice = await _apprenticeRepository.GetById(command.ApprenticeId);
+            var apprentice = await _apprentices.GetById(command.ApprenticeId);
             apprentice.UpdateEmail(new MailAddress(command.Email));
             return Unit.Value;
         }
